b1235737a9eb0acecf427279f41622c8d3038d5b
Aly <alyrashid428@gmail.com>
2025-12-22 00:15:05 +0500

    home page almost complete

---
+++ src/components/OurClients.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class=" bg-grey-11 q-py-xl">
+    <div class="q-my-xl">
+      <div class="text-center">
+        <h2 class="q-my-none font-montserrat-bold font-28">Our clients</h2>
+        <div class="flex flex-center">
+          <div class="elementor-divider"></div>
+        </div>
+        <p class="font-montserrat font-16 q-pt-lg">Driving technology for leading financial brands. <span class="text-bold">self-service portals</span> solution.</p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.elementor-divider {
+    border : 2px solid #e65100;
+    width: 4%;
+    
+}
+</style>
